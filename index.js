b0da638b6d5eface0f14f9212e941b4da0866c5f
melissakipp <melissakipp.az@gmail.com>
2021-05-18 12:07:12 -0700

    database test

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 const express = require("express");
-const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const Campground = require("./models/campground");
 // const methodOverride = require('method-override');
 
 // replace the "test" database with need database name 
-mongoose.connect("mongodb://mongodb:27017/farmStand", { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("Mongo Connection Open")
-  })
-  .catch(err => {
-    console.log("Mongo Connection ERROR: ")
-    console.log(err)
-  });
+mongoose.connect("mongodb://mongodb:27017/yelp-camp", { 
+  useNewUrlParser: true, 
+  useCreateIndex: true, 
+  useUnifiedTopology: true 
+});
 
-app.set("views", path.join(__dirname, "views"));
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const app = express();
+
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 // app.use(methodOverride('_method'))
 
 app.get("/", (req, res) => {
   res.render("home")
+})
+
+// Test
+app.get("/makecampground", async (req, res) => {
+  const camp = new Campground({ title: "My Backyard", description: "cheap camping" });
+  await camp.save();
+  res.send(camp)
 })
 
 app.listen(3000, () => console.log("Server ready"));
