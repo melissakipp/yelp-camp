883827d4f55fc976880771c7ffd6491105e9992e
melissakipp <melissakipp.az@gmail.com>
2021-05-23 16:36:21 -0700

    Created route folder

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate');
-const { campgroundSchema, reviewSchema } = require('./schemas.js');
-const catchAsync = require('./utils/catchAsync');
-const ExpressError = require('./utils/ExpressError');
-const methodOverride = require('method-override');
-const Campground = require('./models/campground');
-const Review = require("./models/review");
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const flash = require("connect-flash");
+const ExpressError = require("./utils/ExpressError");
+const methodOverride = require("method-override");
 
-mongoose.connect('mongodb://mongodb:27017/yelp-camp', {
+
+const campgrounds = require("./routes/campgrounds");
+const reviews = require("./routes/reviews");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 });
 
 const db = mongoose.connection;
@@ -23,108 +26,54 @@ db.once("open", () => {
 
 const app = express();
 
-app.use("/assets",express.static(__dirname + "/assets"));
-app.engine('ejs', ejsMate)
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.engine("ejs", ejsMate)
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
 
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")))
 
-const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
+const sessionConfig = {
+    secret: "thisshouldbeabettersecret!",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
+app.use(session(sessionConfig))
+app.use(flash());
 
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
-
-app.get('/', (req, res) => {
-    res.render('home')
-});
-app.get('/campgrounds', catchAsync(async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds })
-}));
-
-app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new');
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
 })
 
+app.use("/campgrounds", campgrounds)
+app.use("/campgrounds/:id/reviews", reviews)
 
-app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
-    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.get('/campgrounds/:id', catchAsync(async (req, res,) => {
-    const campground = await Campground.findById(req.params.id).populate("reviews");
-    res.render('campgrounds/show', { campground });
-}));
-
-app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/edit', { campground });
-}))
-
-app.put('/campgrounds/:id', validateReview, validateCampground, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    res.redirect(`/campgrounds/${campground._id}`)
-}));
-
-app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds');
-}));
-
-app.post("/campgrounds/:id/reviews", catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    // example of why name="review[body]"
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-}));
-
-// order is important
-app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404))
+app.get("/", (req, res) => {
+    res.render("home")
 });
+
+
+app.all("*", (req, res, next) => {
+    next(new ExpressError("Page Not Found", 404))
+})
 
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
-    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
-    res.status(statusCode).render('error', { err })
-});
+    if (!err.message) err.message = "Oh No, Something Went Wrong!"
+    res.status(statusCode).render("error", { err })
+})
 
 app.listen(3000, () => {
-    console.log('Serving on port 3000')
-});
-
+    console.log("Serving on port 3000")
+})
 // Bret Fisher: Docker for Node.js course Video 25
 // quit on ctrl-c when running docker in terminal
 process.on("SIGINT", function onSigint () {
@@ -151,5 +100,3 @@ function shutdown() {
   })
 };
 // END Bret Fisher
-
-module.exports = app;
