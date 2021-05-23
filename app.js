c517289c6d47674cb58e89b91e2bc07d8421204a
melissakipp <melissakipp.az@gmail.com>
2021-05-22 17:23:16 -0700

    Added basic review form and database logic

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const Review = require("./models/review");
 
 mongoose.connect('mongodb://mongodb:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -82,20 +83,30 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     res.redirect('/campgrounds');
 }));
 
+app.post("/campgrounds/:id/reviews", catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    // example of why name="review[body]"
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
+
 // order is important
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404))
-})
+});
 
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
     if (!err.message) err.message = 'Oh No, Something Went Wrong!'
     res.status(statusCode).render('error', { err })
-})
+});
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
-})
+});
 
 // Bret Fisher: Docker for Node.js course Video 25
 // quit on ctrl-c when running docker in terminal
@@ -108,7 +119,7 @@ process.on("SIGINT", function onSigint () {
 process.on("SIGTERM", function onSigterm () {
   console.info("Got SIGTERM (docker container stop). Graceful shutdown ", new Date().toISOString());
   shutdown();
-})
+});
 
 // shut down server
 function shutdown() {
@@ -121,7 +132,7 @@ function shutdown() {
 		}
 		process.exit();
   })
-}
+};
 // END Bret Fisher
 
 module.exports = app;
