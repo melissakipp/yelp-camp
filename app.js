ba82deee386ab94ed3a0a41ad62280a7ff79892d
melissakipp <melissakipp.az@gmail.com>
2021-05-24 10:49:44 -0700

    add basic auth, currently not working

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const express = require("express");
-const path = require("path");
-const mongoose = require("mongoose");
-const ejsMate = require("ejs-mate");
-const session = require("express-session");
-const flash = require("connect-flash");
-const ExpressError = require("./utils/ExpressError");
-const methodOverride = require("method-override");
-const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const User = require("./models/user");
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
-const userRoutes = require("./routes/users")
-const campgroundRoutes = require("./routes/campgrounds");
-const reviewRoutes = require("./routes/reviews");
 
-mongoose.connect("mongodb://mongodb:27017/yelp-camp", {
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+
+mongoose.connect('mongodb://mongodb:27017/yelp-camp', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
@@ -29,17 +30,16 @@ db.once("open", () => {
 
 const app = express();
 
-app.engine("ejs", ejsMate)
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"))
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname, "public")))
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
 
 const sessionConfig = {
-    // This 
-    secret: "thisshouldbeabettersecret!",
+    secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -48,6 +48,7 @@ const sessionConfig = {
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
+
 app.use(session(sessionConfig))
 app.use(flash());
 
@@ -59,39 +60,38 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
+    console.log(req.session)
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
     next();
 })
 
-app.get("/fakeUser", async (req, res) => {
-    const user = new User({ email: "melissajean@gmail.com", username: "melissajl922"});
-    const newUser = await User.register(user, "children");
-    res.send(newUser);
-})
 
-app.use("/", userRoutes);
-app.use("/campgrounds", campgroundRoutes);
-app.use("/campgrounds/:id/reviews", reviewRoutes);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
 
-app.get("/", (req, res) => {
-    res.render("home")
+
+app.get('/', (req, res) => {
+    res.render('home')
 });
 
 
-app.all("*", (req, res, next) => {
-    next(new ExpressError("Page Not Found", 404))
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
 })
 
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
-    if (!err.message) err.message = "Oh No, Something Went Wrong!"
-    res.status(statusCode).render("error", { err })
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
 })
 
 app.listen(3000, () => {
-    console.log("Serving on port 3000")
+    console.log('Serving on port 3000')
 })
+
 // Bret Fisher: Docker for Node.js course Video 25
 // quit on ctrl-c when running docker in terminal
 process.on("SIGINT", function onSigint () {
