2fe8f98989d51433ef5ef8310201e5622af7612f
melissakipp <melissakipp.az@gmail.com>
2021-05-25 20:56:19 -0700

    add image upload and geocoding for campgrounds

--- database/seeds/index.js
+++ database/seeds/index.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
-const cities = require("./cities");
-const { places, descriptors } = require("./seedHelpers");
-const Campground = require("../../models/campground");
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../../models/campground');
 
-mongoose.connect("mongodb://mongodb:27017/yelp-camp", {
+mongoose.connect('mongodb://mongodb:27017/yelp-camp', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
@@ -11,9 +11,9 @@ mongoose.connect("mongodb://mongodb:27017/yelp-camp", {
 
 const db = mongoose.connection;
 
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database connected");
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
 });
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
@@ -25,12 +25,27 @@ const seedDB = async () => {
             const random1000 = Math.floor(Math.random() * 1000);
             const price = Math.floor(Math.random() * 20) + 10;
             const camp = new Campground({
+                // Your User ID
                 author: '60abbd47a1b89c007d4fe9fe',
                 location: `${cities[random1000].city}, ${cities[random1000].state}`,
                 title: `${sample(descriptors)} ${sample(places)}`,
-                image: "https://source.unsplash.com/collection/482351",
-                description: "Street art scenester edison bulb, letterpress squid vice farm-to-table hoodie chartreuse la croix trust fund kale chips single-origin coffee.",
-                price
+                // image: 'https://source.unsplash.com/collection/482351',
+                description: 'Street art scenester edison bulb, letterpress squid vice farm-to-table hoodie chartreuse la croix trust fund kale chips single-origin coffee.',
+                price,
+                geometry: {
+                    type: "Point",
+                    coordinates: [-113.1331, 47.0202]
+                },
+                image: [
+                    {
+                        url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
+                        filename: 'YelpCamp/ahfnenvca4tha00h2ubt'
+                    },
+                    {
+                        url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png',
+                        filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi'
+                    }
+                ]
             })
             await camp.save();
         }
--- middleware.js
+++ middleware.js
@@ -3,9 +3,9 @@ const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
 const Review = require('./models/review');
 
-
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl
         req.flash('error', 'You must be signed in first!');
         return res.redirect('/login');
     }
@@ -14,6 +14,7 @@ module.exports.isLoggedIn = (req, res, next) => {
 
 module.exports.validateCampground = (req, res, next) => {
     const { error } = campgroundSchema.validate(req.body);
+    console.log(req.body);
     if (error) {
         const msg = error.details.map(el => el.message).join(',')
         throw new ExpressError(msg, 400)
@@ -26,7 +27,7 @@ module.exports.isAuthor = async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     if (!campground.author.equals(req.user._id)) {
-        req.flash('error', 'You are not authorized for this action.');
+        req.flash('error', 'You do not have permission to do that!');
         return res.redirect(`/campgrounds/${id}`);
     }
     next();
@@ -36,7 +37,7 @@ module.exports.isReviewAuthor = async (req, res, next) => {
     const { id, reviewId } = req.params;
     const review = await Review.findById(reviewId);
     if (!review.author.equals(req.user._id)) {
-        req.flash('error', 'You are not authorized for this action.');
+        req.flash('error', 'You do not have permission to do that!');
         return res.redirect(`/campgrounds/${id}`);
     }
     next();
@@ -45,7 +46,7 @@ module.exports.isReviewAuthor = async (req, res, next) => {
 module.exports.validateReview = (req, res, next) => {
     const { error } = reviewSchema.validate(req.body);
     if (error) {
-        const msg = error.details.map(el => el.message).join(",")
+        const msg = error.details.map(el => el.message).join(',')
         throw new ExpressError(msg, 400)
     } else {
         next();
